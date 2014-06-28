b2abdea5c484b01ede469bc93df3bd6b8f4ba330
Alexander Shorin <kxepal@gmail.com>
2014-06-28 16:36:07 +0400

    Add CouchDB docs to intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -266,4 +266,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+    'http://docs.couchdb.org/en/latest': None
+}
