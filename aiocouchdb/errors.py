a1467564460702947e293e7a0021574aa6855069
Alexander Shorin <kxepal@gmail.com>
2014-07-01 07:25:08 +0400

    Fix exception name for Bad Request response

--- aiocouchdb/errors.py
+++ aiocouchdb/errors.py
@@ -26,7 +26,7 @@ class CouchHttpError(BaseException):
         return self.reason
 
 
-class HttpBadRequest(CouchHttpError, HttpException):
+class BadRequest(CouchHttpError, HttpException):
     """The request could not be understood by the server due to malformed
     syntax."""
 
