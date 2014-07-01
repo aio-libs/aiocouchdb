7063348dc8c75d75c01fe8f8ddf6c656face5254
Alexander Shorin <kxepal@gmail.com>
2014-07-01 08:33:05 +0400

    Import the most important things at package level

--- aiocouchdb/__init__.py
+++ aiocouchdb/__init__.py
@@ -6,3 +6,25 @@
 # This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 #
+
+from .authn import (
+    AuthProvider,
+    NoAuthProvider,
+    BasicAuthProvider,
+    CookieAuthProvider,
+    OAuthProvider
+)
+from .errors import (
+    HttpException,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    ResourceNotFound,
+    MethodNotAllowed,
+    ResourceConflict,
+    PreconditionFailed,
+    ServerError,
+    maybe_raise_error
+)
+from .server import Server
+from .version import __version__, __version_info__
