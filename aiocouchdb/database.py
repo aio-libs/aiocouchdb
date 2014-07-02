180c82e19967bbd72438c29bf764987732fc7951
Alexander Shorin <kxepal@gmail.com>
2014-07-03 02:17:14 +0400

    Implement /{db} API call

--- aiocouchdb/database.py
+++ aiocouchdb/database.py
@@ -7,7 +7,10 @@
 # you should have received as part of this distribution.
 #
 
+import asyncio
+
 from .client import Resource
+from .errors import maybe_raise_error
 
 
 class Database(object):
@@ -17,3 +20,63 @@ class Database(object):
         if isinstance(url_or_resource, str):
             url_or_resource = Resource(url_or_resource)
         self.resource = url_or_resource
+
+    @asyncio.coroutine
+    def exists(self, *, auth=None):
+        """Checks if `database exists`_ on server. Assumes success on receiving
+        response with `200 OK` status.
+
+        :param auth: :class:`aiocouchdb.authn.AuthProvider` instance
+
+        :rtype: bool
+
+        .. _database exists: http://docs.couchdb.org/en/latest/api/database/common.html#head--db
+        """
+        resp = yield from self.resource.head(auth=auth)
+        yield from resp.read(close=True)
+        return resp.status == 200
+
+    @asyncio.coroutine
+    def info(self, *, auth=None):
+        """Returns `database information`_.
+
+        :param auth: :class:`aiocouchdb.authn.AuthProvider` instance
+
+        :rtype: dict
+
+        .. _database information: http://docs.couchdb.org/en/latest/api/database/common.html#get--db
+        """
+        resp = yield from self.resource.get(auth=auth)
+        yield from maybe_raise_error(resp)
+        return (yield from resp.json(close=True))
+
+    @asyncio.coroutine
+    def create(self, *, auth=None):
+        """`Creates a database`_.
+
+        :param auth: :class:`aiocouchdb.authn.AuthProvider` instance
+
+        :rtype: bool
+
+        .. _Creates a database: http://docs.couchdb.org/en/latest/api/database/common.html#put--db
+        """
+        resp = yield from self.resource.put(auth=auth)
+        yield from maybe_raise_error(resp)
+        status = yield from resp.json(close=True)
+        return status['ok']
+
+
+    @asyncio.coroutine
+    def delete(self, *, auth=None):
+        """`Deletes a database`_.
+
+        :param auth: :class:`aiocouchdb.authn.AuthProvider` instance
+
+        :rtype: bool
+
+        .. _Deletes a database: http://docs.couchdb.org/en/latest/api/database/common.html#delete--db
+        """
+        resp = yield from self.resource.delete(auth=auth)
+        yield from maybe_raise_error(resp)
+        status = yield from resp.json(close=True)
+        return status['ok']
--- aiocouchdb/tests/test_database.py
+++ aiocouchdb/tests/test_database.py
@@ -28,3 +28,53 @@ class DatabaseTestCase(utils.TestCase):
         server = aiocouchdb.server.Server(res)
         self.assertIsInstance(server.resource, aiocouchdb.client.Resource)
         self.assertEqual(self.url_db, self.db.resource.url)
+
+    def test_exists(self):
+        resp = self.mock_json_response(data=b'{}')
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.db.exists())
+        self.assert_request_called_with('HEAD', 'db')
+        self.assertTrue(result)
+
+    def test_exists_forbidden(self):
+        resp = self.mock_json_response(data=b'{}')
+        resp.status = 403
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.db.exists())
+        self.assert_request_called_with('HEAD', 'db')
+        self.assertFalse(result)
+
+    def test_exists_not_found(self):
+        resp = self.mock_json_response(data=b'{}')
+        resp.status = 404
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.db.exists())
+        self.assert_request_called_with('HEAD', 'db')
+        self.assertFalse(result)
+
+    def test_info(self):
+        resp = self.mock_json_response(data=b'{}')
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.db.info())
+        self.assert_request_called_with('GET', 'db')
+        self.assertIsInstance(result, dict)
+
+    def test_create(self):
+        resp = self.mock_json_response(data=b'{"ok": true}')
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.db.create())
+        self.assert_request_called_with('PUT', 'db')
+        self.assertTrue(result)
+
+    def test_delete(self):
+        resp = self.mock_json_response(data=b'{"ok": true}')
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.db.delete())
+        self.assert_request_called_with('DELETE', 'db')
+        self.assertTrue(result)
