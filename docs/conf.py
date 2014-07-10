a2d24478dd25a311dc58fee241b185807e6ebcb7
Alexander Shorin <kxepal@gmail.com>
2014-07-10 20:56:34 +0400

    Fix docs for RTD service

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import aiocouchdb
 import sys
 import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import aiocouchdb
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
