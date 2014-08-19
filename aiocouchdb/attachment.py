71fe5056f29f6cb161ec968d907659b5b657e7df
Alexander Shorin <kxepal@gmail.com>
2014-08-19 08:49:22 +0400

    Implement HEAD /{db}/{docid}/{attname} API call

--- aiocouchdb/attachment.py
+++ aiocouchdb/attachment.py
@@ -7,6 +7,8 @@
 # you should have received as part of this distribution.
 #
 
+import asyncio
+import base64
 from .client import Resource
 
 
@@ -17,3 +19,48 @@ class Attachment(object):
         if isinstance(url_or_resource, str):
             url_or_resource = Resource(url_or_resource)
         self.resource = url_or_resource
+
+    @asyncio.coroutine
+    def exists(self, rev=None, *, auth=None):
+        """Checks if `attachment exists`_. Assumes success on receiving response
+        with `200 OK` status.
+
+        :param str rev: Document revision
+        :param auth: :class:`aiocouchdb.authn.AuthProvider` instance
+
+        :rtype: bool
+
+        .. _attachment exists: http://docs.couchdb.org/en/latest/api/document/attachments.html#head--db-docid-attname
+        """
+        resp = yield from self.resource.head(auth=auth, params={'rev': rev})
+        yield from resp.read()
+        return resp.status == 200
+
+    @asyncio.coroutine
+    def modified(self, digest, *, auth=None):
+        """Checks if `attachment was modified`_ by known MD5 digest.
+
+        :param bytes digest: Attachment MD5 digest. Optionally,
+                             may be passed in base64 encoding form
+        :param auth: :class:`aiocouchdb.authn.AuthProvider` instance
+
+        :rtype: bool
+
+        .. _attachment was modified: http://docs.couchdb.org/en/latest/api/document/attachments.html#head--db-docid-attname
+        """
+        if isinstance(digest, bytes):
+            if len(digest) != 16:
+                raise ValueError('MD5 digest has 16 bytes')
+            digest = base64.b64encode(digest).decode()
+        elif isinstance(digest, str):
+            if not (len(digest) == 24 and digest.endswith('==')):
+                raise ValueError('invalid base64 encoded MD5 digest')
+        else:
+            raise TypeError('invalid `digest` type {}, bytes or str expected'
+                            ''.format(type(digest)))
+        qdigest = '"%s"' % digest
+        resp = yield from self.resource.head(auth=auth,
+                                             headers={'IF-NONE-MATCH': qdigest})
+        yield from resp.maybe_raise_error()
+        yield from resp.read()
+        return resp.status != 304
--- aiocouchdb/tests/test_attachment.py
+++ aiocouchdb/tests/test_attachment.py
@@ -7,6 +7,9 @@
 # you should have received as part of this distribution.
 #
 
+import base64
+import hashlib
+
 import aiocouchdb.client
 import aiocouchdb.attachment
 import aiocouchdb.tests.utils as utils
@@ -28,3 +31,77 @@ class AttachmentTestCase(utils.TestCase):
         doc = aiocouchdb.attachment.Attachment(res)
         self.assertIsInstance(doc.resource, aiocouchdb.client.Resource)
         self.assertEqual(self.url_doc, self.att.resource.url)
+
+    def test_exists(self):
+        resp = self.mock_json_response()
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.att.exists())
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att')
+        self.assertTrue(result)
+
+    def test_exists_rev(self):
+        resp = self.mock_json_response()
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.att.exists('1-ABC'))
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att',
+                                        params={'rev': '1-ABC'})
+        self.assertTrue(result)
+
+    def test_exists_forbidden(self):
+        resp = self.mock_json_response()
+        resp.status = 403
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.att.exists())
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att')
+        self.assertFalse(result)
+
+    def test_exists_not_found(self):
+        resp = self.mock_json_response()
+        resp.status = 404
+        self.request.return_value = self.future(resp)
+
+        result = self.run_loop(self.att.exists())
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att')
+        self.assertFalse(result)
+
+    def test_modified(self):
+        resp = self.mock_json_response()
+        self.request.return_value = self.future(resp)
+
+        digest = hashlib.md5(b'foo').digest()
+        reqdigest = '"rL0Y20zC+Fzt72VPzMSk2A=="'
+        result = self.run_loop(self.att.modified(digest))
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att',
+                                        headers={'IF-NONE-MATCH': reqdigest})
+        self.assertTrue(result)
+
+    def test_not_modified(self):
+        resp = self.mock_json_response()
+        resp.status = 304
+        self.request.return_value = self.future(resp)
+
+        digest = hashlib.md5(b'foo').digest()
+        reqdigest = '"rL0Y20zC+Fzt72VPzMSk2A=="'
+        result = self.run_loop(self.att.modified(digest))
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att',
+                                        headers={'IF-NONE-MATCH': reqdigest})
+        self.assertFalse(result)
+
+    def test_modified_with_base64_digest(self):
+        resp = self.mock_json_response()
+        self.request.return_value = self.future(resp)
+
+        digest = base64.b64encode(hashlib.md5(b'foo').digest()).decode()
+        reqdigest = '"rL0Y20zC+Fzt72VPzMSk2A=="'
+        result = self.run_loop(self.att.modified(digest))
+        self.assert_request_called_with('HEAD', 'db', 'docid', 'att',
+                                        headers={'IF-NONE-MATCH': reqdigest})
+        self.assertTrue(result)
+
+    def test_modified_invalid_digest(self):
+        self.assertRaises(TypeError, self.run_loop, self.att.modified({}))
+        self.assertRaises(ValueError, self.run_loop, self.att.modified(b'foo'))
+        self.assertRaises(ValueError, self.run_loop, self.att.modified('bar'))
