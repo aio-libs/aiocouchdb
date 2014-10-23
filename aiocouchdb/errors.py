817228ac215f1609ec46d064f726b7230f0bf318
Alexander Shorin <kxepal@gmail.com>
2014-10-23 20:53:21 +0400

    Ensure that HttpException.__str__ always returns string value

--- aiocouchdb/errors.py
+++ aiocouchdb/errors.py
@@ -47,7 +47,7 @@ class HttpErrorException(aiohttp.errors.HttpErrorException):
         self.headers = headers
 
     def __str__(self):
-        return self.reason
+        return str(self.reason)
 
 
 class BadRequest(HttpErrorException):
