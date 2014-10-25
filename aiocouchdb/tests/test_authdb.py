b1b87e6d38cb7ce44e2f4384f3414a9f7e657fc6
Alexander Shorin <kxepal@gmail.com>
2014-10-25 22:46:05 +0400

    Adopt authdb tests to run against CouchDB instance

--- aiocouchdb/tests/test_authdb.py
+++ aiocouchdb/tests/test_authdb.py
@@ -7,6 +7,9 @@
 # you should have received as part of this distribution.
 #
 
+import asyncio
+import json
+
 import aiocouchdb.authdb
 
 from aiocouchdb.client import urljoin
@@ -32,49 +35,61 @@ class UserDocumentTestCase(utils.TestCase):
 
     def setUp(self):
         super().setUp()
-        docid = aiocouchdb.authdb.UserDocument.doc_prefix + 'username'
+        self.username = utils.uuid()
+        docid = aiocouchdb.authdb.UserDocument.doc_prefix + self.username
         self.url_doc = urljoin(self.url, '_users', docid)
         self.doc = aiocouchdb.authdb.UserDocument(self.url_doc, docid=docid)
+
+    def tearDown(self):
+        self.loop.run_until_complete(self.teardown_document())
+        super().tearDown()
+
+    @asyncio.coroutine
+    def setup_document(self, password, **kwargs):
+        data = {
+            '_id': self.doc.id,
+            'name': self.doc.name,
+            'password': password,
+            'roles': [],
+            'type': 'user'
+        }
+        data.update(kwargs)
+        with self.response(data=b'{}'):
+            resp = yield from self.doc.register(password, **kwargs)
+        self.assert_request_called_with('PUT', '_users', self.doc.id, data=data)
+        self.assertIsInstance(resp, dict)
+        return resp
+
+    @asyncio.coroutine
+    def teardown_document(self):
+        if not (yield from self.doc.exists()):
+            return
+        with self.response(headers={'ETAG': '"1-ABC"'}):
+            rev = yield from self.doc.rev()
+        yield from self.doc.delete(rev)
 
     def test_require_docid(self):
         with self.assertRaises(ValueError):
             aiocouchdb.authdb.UserDocument(self.url_doc)
 
     def test_username(self):
-        self.assertEqual(self.doc.name, 'username')
+        self.assertEqual(self.doc.name, self.username)
 
     def test_register(self):
-        yield from self.doc.register('s3cr1t')
-        self.assert_request_called_with(
-            'PUT', '_users', self.doc.id,
-            data={
-                '_id': self.doc.id,
-                'name': self.doc.name,
-                'password': 's3cr1t',
-                'roles': [],
-                'type': 'user'
-            })
+        yield from self.setup_document('s3cr1t')
 
     def test_register_with_additional_data(self):
-        yield from self.doc.register('s3cr1t', email='user@example.com')
-        self.assert_request_called_with(
-            'PUT', '_users', self.doc.id,
-            data={
-                '_id': self.doc.id,
-                'email': 'user@example.com',
-                'name': self.doc.name,
-                'password': 's3cr1t',
-                'roles': [],
-                'type': 'user'
-            })
+        yield from self.setup_document('s3cr1t', email='user@example.com')
 
     def test_change_password(self):
-        with self.response(data=b'{"existed": "field"}'):
-            yield from self.doc.update_password('s3cr1t')
+        yield from self.setup_document('s3cr1t')
+        with self.response(data=b'{}'):
+            doc = yield from self.doc.get()
+        data = json.dumps(doc).encode()
+
+        with self.response(data=data):
+            yield from self.doc.update_password('n3ws3cr1t')
+            doc['password'] = 'n3ws3cr1t'
             self.assert_request_called_with(
                 'PUT', '_users', self.doc.id,
-                data={
-                    'existed': 'field',
-                    'password': 's3cr1t'
-                })
-
+                data=doc)
