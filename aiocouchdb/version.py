e5839140d853d0d178a540566cc06cc1ef3fe064
Alexander Shorin <kxepal@gmail.com>
2014-11-12 11:36:07 +0300

    Start work on 0.7 release

--- aiocouchdb/version.py
+++ aiocouchdb/version.py
@@ -7,7 +7,7 @@
 # you should have received as part of this distribution.
 #
 
-__version_info__ = (0, 6, 0, '', 0)
+__version_info__ = (0, 7, 0, 'dev', 0)
 __version__ = '%(version)s%(tag)s%(build)s' % {
     'version': '.'.join(map(str, __version_info__[:3])),
     'tag': '-' + __version_info__[3] if __version_info__[3] else '',
