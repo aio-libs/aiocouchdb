baa0854554238f7bb80554acb8f110f2fd81a08d
Alexander Shorin <kxepal@gmail.com>
2016-02-03 05:55:30 +0300

    Start work on 0.9.1 version

--- aiocouchdb/version.py
+++ aiocouchdb/version.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2014-2015 Alexander Shorin
+# Copyright (C) 2014-2016 Alexander Shorin
 # All rights reserved.
 #
 # This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 #
 
-__version_info__ = (0, 9, 0, '', 0)
+__version_info__ = (0, 9, 1, 'dev', 0)
 __version__ = '%(version)s%(tag)s%(build)s' % {
     'version': '.'.join(map(str, __version_info__[:3])),
     'tag': '-' + __version_info__[3] if __version_info__[3] else '',
