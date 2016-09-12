84a08cc339c430175c069b894b3e27b034f45b12
Alexander Shorin <kxepal@gmail.com>
2016-09-12 22:42:39 +0300

    Suppress flake8 cry for unused imports in proxy module

--- aiocouchdb/hdrs.py
+++ aiocouchdb/hdrs.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015 Alexander Shorin
+# Copyright (C) 2015-2016 Alexander Shorin
 # All rights reserved.
 #
 # This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 #
+
+# flake8: noqa
 
 from aiohttp.hdrs import *
 from aiohttp.multidict import upstr
